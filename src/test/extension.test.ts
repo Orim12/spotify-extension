7c04197adfca401ff0aa3826fddee7591a932f91
Orim12(Miro Vaassen) <miro@mirovaassen.nl>
2026-02-01 21:57:49 +0100

    Refactor README and test files for clarity and improved structure

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,21 @@
-import * as assert from 'assert';
+import * as assert from "assert";
+import * as vscode from "vscode";
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+suite("Extension Test Suite", () => {
+	const extensionId = "spotify-controller";
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+	test("Extension activates", async () => {
+		const extension = vscode.extensions.getExtension(extensionId);
+		assert.ok(extension, "Extension not found");
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+		await extension!.activate();
+		assert.strictEqual(extension!.isActive, true);
+	});
+
+	test("Commands are registered", async () => {
+		const commands = await vscode.commands.getCommands(true);
+		assert.ok(commands.includes("spotify.playpause"));
+		assert.ok(commands.includes("spotify.next"));
+		assert.ok(commands.includes("spotify.previous"));
 	});
 });
